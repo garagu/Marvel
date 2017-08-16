e01d66b0f299d429a50521394f277ce5985cb114
garagu <garagudev@gmail.com>
2017-08-16 13:13:48 +0200

    Minor details

--- app/src/main/java/com/garagu/marvel/presentation/comic/di/ComicModule.java
+++ app/src/main/java/com/garagu/marvel/presentation/comic/di/ComicModule.java
@@ -2,12 +2,12 @@ package com.garagu.marvel.presentation.comic.di;
 
 import com.garagu.marvel.data.datasource.ComicDatasource;
 import com.garagu.marvel.data.datasource.ReviewDatasource;
-import com.garagu.marvel.data.datasource.local.ComicLocalDatasource;
+import com.garagu.marvel.data.datasource.remote.ComicRemoteDatasource;
 import com.garagu.marvel.data.datasource.remote.ReviewRemoteDatasource;
-import com.garagu.marvel.data.local.FileManager;
 import com.garagu.marvel.data.mapper.ComicDateEntityMapper;
 import com.garagu.marvel.data.mapper.ComicEntityMapper;
 import com.garagu.marvel.data.mapper.ReviewEntityMapper;
+import com.garagu.marvel.data.net.MarvelApi;
 import com.garagu.marvel.data.repository.ComicDataRepository;
 import com.garagu.marvel.data.repository.ReviewDataRepository;
 import com.garagu.marvel.domain.repository.ComicRepository;
@@ -16,7 +16,6 @@ import com.garagu.marvel.presentation.application.di.ActivityScope;
 import com.garagu.marvel.presentation.comic.model.ComicModelMapper;
 import com.garagu.marvel.presentation.comic.view.Navigator;
 import com.garagu.marvel.presentation.common.model.ReviewModelMapper;
-import com.google.gson.Gson;
 
 import dagger.Module;
 import dagger.Provides;
@@ -56,20 +55,19 @@ public class ComicModule {
     ReviewModelMapper provideReviewModelMapper() {
         return new ReviewModelMapper();
     }
-
+/*
     @Provides
     @ActivityScope
     ComicDatasource provideComicDatasource(FileManager fileManager, Gson gson) {
         return new ComicLocalDatasource(fileManager, gson);
     }
+*/
+    @Provides
+    @ActivityScope
+    ComicDatasource provideComicDatasource(MarvelApi marvelApi) {
+        return new ComicRemoteDatasource(marvelApi);
+    }
 
-    /*
-        @Provides
-        @ActivityScope
-        ComicDatasource provideComicDatasource(MarvelApi marvelApi) {
-            return new ComicRemoteDatasource(marvelApi);
-        }
-    */
     @Provides
     @ActivityScope
     ComicEntityMapper provideComicEntityMapper() {
